1185797c8d751fd975a82de643b36c7c979dfefe
Tychio <code@tychio.net>
2013-09-27 17:41:21 +0800

    add the clock plate

--- js/timer.js
+++ js/timer.js
@@ -1,44 +1,86 @@
 (function (undefined) {
     "use strict";
-    var time,value;
-    var timer = setInterval(function () {
-        time = new Date();
-        value = {
-            meridiem: time.getHours() > 12,
-            hour: time.getHours() % 12,
-            minute: time.getMinutes(),
-            second: time.getSeconds()
-        };
-        setTimer(value);
-    }, 300);
-    function setTimer (p_value) {
-        // text part
-        setTimeText(p_value);
-        // cycle part
+    var SECOND_WIDTH = 4; // deg
+    var MINUTE_WIDTH = 10; // deg
+    var HOUR_WIDTH = 30; // deg
+    var ACCURACY = 40; //ms
+    var PREFIXS = ['', 'moz', 'webkit']; // browers
+    // run
+    var timer = setInterval(tricle, ACCURACY);
+    function tricle () {
+        var time = new Date();
+        var seconds = time.getSeconds();
+        var minutes = time.getMinutes();
+        var hours = time.getHours();
+        var milliseconds = time.getMilliseconds();
+        setTimer(milliseconds, seconds, minutes, hours);
     }
-    function setTimeText (p_value) {
+    // set text and plate
+    function setTimer (p_milliseconds, p_seconds, p_minutes, p_hours) {
+        // text part
+        setTimeText({
+            second: p_seconds,
+            minute: p_minutes,
+            hour: p_hours % 12,
+            meridiem: p_hours > 12
+        });
+        // plate part
+        setTimePlate({
+            second: p_seconds + p_milliseconds / 1000,
+            minute: p_minutes + p_seconds / 60,
+            hour: p_hours + p_minutes / 60
+        });
+    }
+    function setTimePlate (p_time) {
+        var selector = {
+            second: '.point_second',
+            minute: '.point_minute',
+            hour: '.point_hour'
+        };
+        _setTimePoint(p_time.second, SECOND_WIDTH, selector.second, 60);
+        _setTimePoint(p_time.minute, MINUTE_WIDTH, selector.minute, 60);
+        _setTimePoint(p_time.hour, HOUR_WIDTH, selector.hour, 12);
+        function _setTimePoint (p_time, p_width, p_selector, p_scope) {
+            var rotate = p_time*(360/p_scope) - 90 + p_width*0.5;
+            var skew = p_width - 90;
+            var transform = 'rotate(' + rotate + 'deg) skew(' + skew + 'deg)';
+            for (var i = 0; i < PREFIXS.length; i++) {
+                var _prefix = PREFIXS[i] === '' ? '' : '-' + PREFIXS[i] + '-';
+                var _dom = document.querySelector(p_selector);
+                _dom.style[_prefix + 'transform'] = transform;
+            }
+        }
+    }
+    function setTimeText (p_time) {
         var selector = {
             meridiem:         '.time_meridiem',
             hour:             '.time_hour',
             minute:           '.time_minute',
             second:           '.time_second'
         };
-        _setTimeValue(selector.meridiem, p_value.meridiem ? 'AM' : 'PM');
-        _setTimeValue(selector.hour, coverage(p_value.hour));
-        _setTimeValue(selector.minute, coverage(p_value.minute));
-        _setTimeValue(selector.second, coverage(p_value.second));
+        _setTimeValue(selector.meridiem, p_time.meridiem ? 'AM' : 'PM');
+        _setTimeValue(selector.hour, coverage(p_time.hour));
+        _setTimeValue(selector.minute, coverage(p_time.minute));
+        _setTimeValue(selector.second, coverage(p_time.second));
         function _setTimeValue (p_selector, p_value) {
-            document.querySelectorAll(p_selector)[0].innerHTML = p_value;
+            document.querySelector(p_selector).innerHTML = p_value;
         }
     }
-    function coverage (p_number, p_unit_size) {
+    /**
+     * complement digits
+     * @param p_number[number] the number to be processed.
+     * @param p_size[number] digits size, default is 2.
+     * @return [string] don't try to parse it to number,
+     *     maybe there is '0' in its front.
+    **/
+    function coverage (p_number, p_size) {
         p_number += '';// parse to string
-        if (p_unit_size === undefined) {// default size is 2
-            p_unit_size = 2;
+        if (p_size === undefined) {// default size is 2
+            p_size = 2;
         }
-        for (var i = 0; i < p_unit_size; i++) {// cover zero
+        for (var i = 0; i < p_size; i++) {// cover zero
             p_number = '0' + p_number;
         }
-        return p_number.slice(0 - p_unit_size);
+        return p_number.slice(0 - p_size);
     }
 })();
