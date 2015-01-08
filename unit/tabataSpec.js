791b30ed075ab5afa6ea35cf3d9d8cc17924998f
Tychio <code@tychio.net>
2015-01-08 18:08:54 +0800

    add unit test for tabata api of show

--- unit/tabataSpec.js
+++ unit/tabataSpec.js
@@ -1,9 +1,22 @@
 describe('Tabata', function () {
-    var count, dial, sky;
+	var count, round, roundNum;
+    function facultyCount () {
+        var counter = jasmine.createSpyObj('counter', ['init', 'prefix', 'set', 'show']);
+        counter.init.and.returnValue(counter);
+        counter.prefix.and.returnValue(counter);
+        counter.set.and.returnValue(counter);
+
+        return counter;
+    }
+
     beforeEach(function () {
-        count = jasmine.createSpyObj('count', ['init', 'prefix', 'set']);
-        count.prefix.and.returnValue(count);
-        spyOn(window, 'Count').and.returnValue(count);
+    	var counter = {};
+    	count = counter['tabata'] = facultyCount();
+    	roundNum = counter['tabata-round-num'] = facultyCount();
+    	round = counter['tabata-round'] = facultyCount();
+        spyOn(window, 'Count').and.callFake(function (params) {
+        	return counter[params.name];
+        });
 
 		window.Audio = jasmine.createSpy('audio');
 		window.Audio.and.callFake(function (param) { return param; });
@@ -26,19 +39,17 @@ describe('Tabata', function () {
 				tabata.init();
 			});
 
-			it('init three times', function () {
-				expect(count.init.calls.count()).toEqual(3);
-			});
-
-			it('count', function () {
+			it('should init count', function () {
 				expect(Count).toHaveBeenCalledWith({
 					name: 'tabata',
 					className: 'tabataTestName',
                 	handler: '.time-cycle'
 				});
+
+				expect(count.init).toHaveBeenCalled();
 			});
 
-			it('round number', function () {
+			it('should init round number', function () {
 				expect(Count).toHaveBeenCalledWith({
 					name: 'tabata-round-num',
                 	handler: '#count-set',
@@ -46,9 +57,11 @@ describe('Tabata', function () {
                 	division: '',
                 	hidden: true
 				});
+
+				expect(roundNum.init).toHaveBeenCalled();
 			});
 
-			it('round', function () {
+			it('should init round', function () {
 				expect(Count).toHaveBeenCalledWith({
 		            name: 'tabata-round',
 		            handler: '#count-set',
@@ -56,6 +69,35 @@ describe('Tabata', function () {
 		            division: '',
 		            hidden: true
 				});
+
+				expect(round.init).toHaveBeenCalled();
+			});
+		});
+
+		describe('show', function () {
+			beforeEach(function () {
+				tabata = Tabata();
+				tabata.init();
+			});
+
+			it('should init value of count', function () {
+				tabata.show();
+				expect(count.set).toHaveBeenCalledWith(0);
+			});
+
+			it('should show count component', function () {
+				tabata.show();
+				expect(count.show).toHaveBeenCalled();
+			});
+
+			it('should show round number component', function () {
+				tabata.show();
+				expect(roundNum.show).toHaveBeenCalled();
+			});
+
+			it('should show round component', function () {
+				tabata.show();
+				expect(round.show).toHaveBeenCalled();
 			});
 		});
 	});
